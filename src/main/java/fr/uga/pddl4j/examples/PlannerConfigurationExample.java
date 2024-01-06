6e52d905298f9678bc585cc6c15331406b683097
xie <875876184@qq.com>
2024-01-06 18:13:45 +0800

    11

--- src/main/java/fr/uga/pddl4j/examples/PlannerConfigurationExample.java
+++ src/main/java/fr/uga/pddl4j/examples/PlannerConfigurationExample.java
@@ -42,33 +42,37 @@ public class PlannerConfigurationExample {
     public static void main(String[] args) {
 
         // The path to the benchmarks directory
-        //final String benchmarks = "src\\test\\resources\\benchmarks\\hddl\\ipc2020\\satellite\\";
+        // final String benchmarks =
+        // "src\\test\\resources\\benchmarks\\hddl\\ipc2020\\satellite\\";
 
         String path = "D:\\git workspace\\Tex_WorkSpace\\001 SpaceStationMissionPlanning\\002 Experiment&Result\\Instance\\";
-
-
 
         // Gets the default configuration from the planner
         fr.uga.pddl4j.planners.PlannerConfiguration config = HSP.getDefaultConfiguration();
         // Sets the domain of the problem to solve
         config.setProperty(HSP.DOMAIN_SETTING, path + "domain.pddl");
         // Sets the problem to solve
-        config.setProperty(HSP.PROBLEM_SETTING, path+ "p01.pddl");
+        config.setProperty(HSP.PROBLEM_SETTING, path + "p01.pddl");
         // Sets the timeout allocated to the search.
-        config.setProperty(HSP.TIME_OUT_SETTING, 1000);
-        // Sets the log level
-        config.setProperty(HSP.LOG_LEVEL_SETTING, LogLevel.INFO);
+        config.setProperty(HSP.TIME_OUT_SETTING, 100);// 100s
+        //  Set the level of trace of the planner: ALL, DEBUG,
+        // INFO, ERROR, FATAL, OFF, TRACE (preset INFO)
+        config.setProperty(HSP.LOG_LEVEL_SETTING, LogLevel.DEBUG); // 有几种log的level，根据需要返回需要的值
+
         // Sets the heuristic used to search
-        //config.setProperty(TFD.HEURISTIC_SETTING, StateHeuristic.Name.MAX);
-        //Sets the weight of the heuristic
-        //config.setProperty(TFD.WEIGHT_HEURISTIC_SETTING, 1.2);
+        // config.setProperty(TFD.HEURISTIC_SETTING, StateHeuristic.Name.MAX);
+        // Sets the weight of the heuristic
+        // config.setProperty(TFD.WEIGHT_HEURISTIC_SETTING, 1.2);
 
         // Creates an instance of the HSP planner with the specified configuration
         Planner planner = Planner.getInstance(Planner.Name.HSP, config);
+        
 
         // Runs the planner and print the solution
         try {
             planner.solve();
+            // planner.getLogLevel().toString();
+            
         } catch (InvalidConfigurationException e) {
             e.printStackTrace();
         }
